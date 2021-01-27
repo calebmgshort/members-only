1b2b706743ed6bc3569d79ad8cce4db5abedafe2
Caleb Short <calebshort4697@gmail.com>
2021-01-26 20:09:26 -0500

    Finished

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,34 @@
 class PostsController < ApplicationController
 
+  before_action :authenticate_user!, only: [:new, :create]
+
   def new
+    @post = Post.new
   end
 
   def create
+    logger.debug user_signed_in?
+    logger.debug current_user
+    @post = current_user.posts.build(post_params)
+    # logger.debug @post.user
+
+    if @post.save
+      logger.debug "succeeded"
+      redirect_to posts_url
+    else
+      logger.debug "failed"
+      logger.debug @post.errors.full_messages
+      render :new
+    end
   end
 
   def index
+    @posts = Post.all
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:body)
   end
   
 end
